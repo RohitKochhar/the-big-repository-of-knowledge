319a1ff27d0ad83f045c46ba6848ba68d74cf322
Rohit Singh <rkochhar@uwaterloo.ca>
2022-06-21 20:59:33 -0400

    finished t3

---
+++ RabbitMQ/tutorials/publish_subscribe/log_consumer.py
@@ -0,0 +1,50 @@
+# File Information ---------------------------------------------
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+# 	File Name: log_consumer.py
+#
+# 	File Description: 
+# 
+# 	File History:
+# 		- 2022-06-21: Created by Rohit S.
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+
+# Imports --------------------------------------------------------
+import time, pika, sys, os
+sys.path.append('../')
+from RabidsMQ import RabidsMQ
+
+# Global Variables -----------------------------------------------
+
+# Class Declarations ---------------------------------------------
+
+# Function Declarations ------------------------------------------
+
+# Main Call ------------------------------------------------------
+if __name__ == '__main__':
+    print('Running log_consumer')
+    try:
+        # Define Rabids MQ instance
+        o_Rabids = RabidsMQ(s_Host='localhost')
+        # Open channel 
+        o_Rabids.openChannel()
+        # Declare exchange
+        o_Rabids.o_Channel.exchange_declare(exchange='logs', exchange_type='fanout')
+        # Store the resulting queue name
+        s_QueueName = o_Rabids.o_Channel.queue_declare(queue='', exclusive=True).method.queue
+        # Bind the channel's queue to the exchange
+        o_Rabids.o_Channel.queue_bind(exchange='logs', queue=s_QueueName)
+        # Wait for logs
+        print("Waiting for logs...")
+        o_Rabids.basicConsume(
+            s_Queue=s_QueueName,
+            F_Callback= (lambda ch, method, properties, body: body),
+            b_AutoAck=True
+        )
+        # Start consuming
+        o_Rabids.o_Channel.start_consuming()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ RabbitMQ/tutorials/publish_subscribe/log_emitter.py
@@ -0,0 +1,43 @@
+# File Information ---------------------------------------------
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+# 	File Name: log_emitter.py
+#
+# 	File Description: Program to send logs to RMQ
+# 
+# 	File History:
+# 		- 2022-06-21: Created by Rohit S.
+# . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+
+# Imports --------------------------------------------------------
+import time, pika, sys, os
+sys.path.append('../')
+from RabidsMQ import RabidsMQ
+
+# Global Variables -----------------------------------------------
+
+# Class Declarations ---------------------------------------------
+
+# Function Declarations ------------------------------------------
+
+# Main Call ------------------------------------------------------
+if __name__ == '__main__':
+    print('Running log_emitter')
+    # Define RabidsMQ instance
+    o_Rabids = RabidsMQ(s_Host='localhost')
+    # Open channel
+    o_Rabids.openChannel()
+    # Declare the exchange
+    o_Rabids.o_Channel.exchange_declare(
+        exchange='logs',
+        exchange_type='fanout'
+    )
+    # Create message
+    s_Message = ' '.join(sys.argv[1:]) or "Default Log"
+    # Publish to the exchange
+    o_Rabids.basicPublish(
+        s_Exchange      = 'logs',
+        s_RoutingKey    = '',
+        s_Body          = s_Message
+    )
+    # Close the queue
+    o_Rabids.o_Channel.close()
